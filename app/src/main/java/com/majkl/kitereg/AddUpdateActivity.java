ab1365f145f2b751814a96fef49b1450f0980a4e
Majkl <majkl.andracic@gmail.com>
2020-04-12 20:00:19 +0200

    Layout Activity add and update

---
+++ app/src/main/java/com/majkl/kitereg/AddUpdateActivity.java
@@ -0,0 +1,63 @@
+package com.majkl.kitereg;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AddUpdateActivity extends AppCompatActivity {
+
+    private CircleImageView profilePic;
+    private EditText nameTxt, phoneTxt, emailTxt, dobTxt, remarkTxt;
+    private FloatingActionButton saveBtn;
+
+    //actionbar
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_update);
+
+        //initialise
+        actionBar = getSupportActionBar();
+
+        //title of ActionBar
+        actionBar.setTitle("AddRecord");
+
+        //back button
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+
+        //initialise views
+        profilePic = findViewById(R.id.profile_pic);
+        nameTxt = findViewById(R.id.nameTxt);
+        phoneTxt = findViewById(R.id.phoneTxt);
+        emailTxt = findViewById(R.id.emailTxt);
+        dobTxt = findViewById(R.id.dobTxt);
+        remarkTxt = findViewById(R.id.remarkTxt);
+        saveBtn = findViewById(R.id.saveBtn);
+
+        //clicking to show image
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //clicking to save
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/majkl/kitereg/MainActivity.java
+++ app/src/main/java/com/majkl/kitereg/MainActivity.java
@@ -2,13 +2,34 @@ package com.majkl.kitereg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    //views
+    private FloatingActionButton addRecordbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize views
+        addRecordbtn = findViewById(R.id.addRecordBtn);
+
+        //on click start AddUpdate activity
+        addRecordbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //starting AddUpdate activity
+                startActivity(new Intent(MainActivity.this,AddUpdateActivity.class));
+
+            }
+
+        });
     }
 }
